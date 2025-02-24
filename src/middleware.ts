dc44f33272cb983e77bbbb30a5e032fb87c3631f
norriswu0 <norris.wu.au+github@outlook.com>
2025-02-24 06:49:19 +0000

    clear up

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,18 @@
-
 import NextAuth from "next-auth";
-import { NextRequest } from "next/server";
 import { baseConfig } from "./system/auth.config";
 
 /**
  * Middleware are running in the edge runtime
- * 
+ *
  * before `runtime: 'nodejs'` is out of canary, edge compatibility is required
  * https://github.com/vercel/next.js/pull/75624
- * 
+ *
  */
 
-export const { auth } = NextAuth(baseConfig)
+export const { auth } = NextAuth(baseConfig);
 
-export default auth((req: NextRequest) => {
-  console.log("middleware", req);
-});
-
+export default auth;
 
 export const config = {
-  matcher: [
-    "/dashboard",
-    "/dashboard/:path*",
-  ],
+  matcher: ["/dashboard", "/dashboard/:path*"],
 };

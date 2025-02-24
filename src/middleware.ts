5aaaf46a8f73cf6ae77eea04f639e8ff83186df3
norriswu0 <norris.wu.au+github@outlook.com>
2025-02-24 02:49:11 +0000

    add note re middleware edge compatibility

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,13 @@ import NextAuth from "next-auth";
 import { NextRequest } from "next/server";
 import { baseConfig } from "./system/auth.config";
 
+/**
+ * Middleware are running in the edge runtime
+ * 
+ * before `runtime: 'nodejs'` is out of canary, edge compatibility is required
+ * https://github.com/vercel/next.js/pull/75624
+ * 
+ */
 
 export const { auth } = NextAuth(baseConfig)
 
